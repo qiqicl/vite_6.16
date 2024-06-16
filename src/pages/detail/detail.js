0b221bd1b1a59831015857846e265bc3ba6fd5a3
qiqi <934648695@qq.com>
2024-06-17 00:30:55 +0800

    Updates

--- src/pages/detail/detail.js
+++
@@ -1,9 +0,0 @@
-import "./detail.scss"
-import {
-    $,
-    $all
-} from "../../utils/utils.js"
-let data = JSON.parse(localStorage.getItem("index"))
-console.log(data)
-$("h1").innerHTML = data.title;
-$("img").src = data.img
